9ce3fd8afb4325bdfc411d6b59adbd2d8ec0cf52
Hoàng Minh Đức <hoangminhduc.als@gmail.com>
2025-09-29 01:04:05 +0700

    Preprocess data
    - Added new script file "preprocess.py"
    - Standardized column names
    - Pivoted language columns into separate score columns
    - Added province code column (MA_TINH)

---
+++ scripts/preprocess.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+# Ensure code runs regardless of CWD
+ROOT = Path(__file__).resolve().parent.parent
+DATA = ROOT / "data"
+
+# Read raw CSV dataset
+path = DATA / "raw" / "raw.csv"
+raw_data = pd.read_csv(path, dtype={"SOBAODANH": str})
+
+# Standardize column names
+raw_data = raw_data.rename(columns={
+    "SOBAODANH": "SBD",
+    "Toán": "TOAN",
+    "Văn": "VAN",
+    "Lí": "LI",
+    "Hóa": "HOA",
+    "Sinh": "SINH",
+    "Tin học": "TIN",
+    "Công nghệ công nghiệp": "CNCN",
+    "Công nghệ nông nghiệp": "CNNN",
+    "Sử": "SU",
+    "Địa": "DIA",
+    "Giáo dục kinh tế và pháp luật": "KTPL",
+    "Ngoại ngữ": "NN",
+    "Mã môn ngoại ngữ": "MA_NN"
+})
+
+# Pivot language columns into separate score columns
+raw_data["MA_NN"] = raw_data["MA_NN"].map({
+    "N1": "T_ANH",
+    "N2": "T_NGA",
+    "N3": "T_PHAP",
+    "N4": "T_TRUNG",
+    "N5": "T_DUC",
+    "N6": "T_NHAT",
+    "N7": "T_HAN"
+})
+lang_score = raw_data.pivot(columns="MA_NN", values="NN")
+lang_score = lang_score.drop(columns=[np.nan], errors="ignore")
+raw_data = pd.concat([raw_data.drop(columns=["MA_NN", "NN"]), lang_score], axis=1)
+
+# Add province code column
+raw_data.insert(2, "MA_TINH", raw_data["SBD"].str[:2])
+
+# Return processed CSV file
+out_path = DATA / "processed" / "processed.csv"
+raw_data.to_csv(out_path, index=False)
